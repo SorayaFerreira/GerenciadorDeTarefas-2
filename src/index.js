a5ffc2231bd125a6451b5637f64808a52af57729
Kauan Cardoso <kauancardososilva@gmail.com>
2025-06-06 22:30:55 -0300

    feat: adicao do componente FrequentWords no index.js de renderizacao

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './App.css';
+import FrequentWords from '../FrequentWords';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
+<FrequentWords tasks={tasks} />
 root.render(<App />);
